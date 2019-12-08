e433ae8a31953d32838a709dbe80d210ec6b369c
7kaji <hirotonbuu@gmail.com>
2019-12-09 01:47:14 +0900

    markers

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,3 +1,10 @@
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -7,6 +14,16 @@ export class CustomMap {
       center: {
         lat: 0,
         lng: 0
+      }
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
       }
     });
   }
